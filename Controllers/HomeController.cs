32e661a5b9374136ff48e625f83fb716844a13ce
Kc Rose <musa.kcrose.05272003@gmail.com>
2023-09-23 19:37:30 +0800

    changed the content of table to family list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,9 +6,12 @@ public class HomeController : Controller
     {
         var studentInfoArray = new[]
         {
-            new StudentInfoModel { Name = "John Doe", StudentId = "S001", StudentAge = "20" },
-            new StudentInfoModel { Name = "Jane Smith", StudentId = "S002", StudentAge = "22" },
-            new StudentInfoModel { Name = "Mike Johnson", StudentId = "S003", StudentAge = "31" }
+            new StudentInfoModel { Name = "Musa, Kc Rose", ID = "21-1605", StudentAge = "20" },
+            new StudentInfoModel { Name = "Musa, Bianca Mae", ID = "21-1606", StudentAge = "19" },
+            new StudentInfoModel { Name = "Musa, Jaybe", ID = "21-1607", StudentAge = "17" },
+            new StudentInfoModel { Name = "Musa, Myra Aliman", ID = "21-1608", StudentAge = "49" },
+            new StudentInfoModel { Name = "Musa, Nasser Tanangao", ID = "21-1609", StudentAge = "50" }
+            
         };
 
         return View(studentInfoArray);
--- Models/StudentInfoModel.cs
+++ Models/StudentInfoModel.cs
@@ -1,6 +1,6 @@
 public class StudentInfoModel
 {
-    public string Name { get; set; }
-    public string StudentId { get; set; }
-    public string StudentAge { get; set; }
+    public string Name { get; set; } 
+    public string ID { get; set; } 
+    public string StudentAge { get; set; } 
 }
